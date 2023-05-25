777c9d16232b9c6f6b97ac7ff981a753f5517840
Wilson <110120440+AMIS-Labs@users.noreply.github.com>
2023-05-26 01:50:04 +0200

    Update auto_gmail_responder.py

--- auto_gmail_responder.py
+++ auto_gmail_responder.py
@@ -16,10 +16,9 @@ import openai
 # Informations d'identification Gmail
 GMAIL_ADDRESS = "questions-alix@iassurpro.com"
 EMAIL_ADDRESS = "questions-alix@iassurpro.com"
-GMAIL_APP_PASSWORD = "ifcqvrxgkslcizbm"
-APP_PASSWORD = "ifcqvrxgkslcizbm"
+GMAIL_APP_PASSWORD = "cvyolriqdmaehgeu"
 IMAP_SERVER = "imap.gmail.com"
-PASSWORD = "EM*8@DM#j!ak4h$564x"
+PASSWORD = "cvyolriqdmaehgeu"
 
 # Clé secrète OpenAI
 openai.api_key = os.environ["OPENAI_API_KEY"]
