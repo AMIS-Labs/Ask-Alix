318b5535f9a857578db0e87731bb3f3c297a2d86
Wilson <110120440+AMIS-Labs@users.noreply.github.com>
2023-06-08 19:05:50 +0200

    Update ask_alix_iassur_bot.py

--- ask_alix_iassur_bot.py
+++ ask_alix_iassur_bot.py
@@ -14,7 +14,6 @@ from dotenv import load_dotenv
 import requests
 import openai
 import schedule
-import time
 
 # Informations d'identification Gmail
 GMAIL_ADDRESS = "questions-alix@iassurpro.com"
@@ -42,8 +41,9 @@ BOT_NAME = "Alix"
 INTRODUCTION_SENTENCE = "Je suis Alix, votre assistante virtuelle. Je suis une intelligence artificielle produite par IASSUR dans le but de vous aider au sujet de vos assurances professionnelles."
 GREETING_SENTENCE = "J'espère que vous vous portez bien, je ferai mon maximum pour vous aider aujourd'hui !"
 POLITE_CLOSING = "N'hésitez pas à me poser d'autres questions. Je suis là pour vous aider."
-POST_SCRIPTUM = "Veuillez noter que cette réponse est simulée et basée sur mes connaissances générales. Il est donc toujours conseillé de vérifier les sources officielles à jour"
+POST_SCRIPTUM = "Veuillez noter que cette réponse est simulée et basée sur mes connaissances générales. Il est donc toujours conseillé de vérifier les sources officielles à jour."
 
+# Fonction pour envoyer un e-mail Gmail
 def send_gmail(sender_address, receiver_address, mail_subject, mail_content):
     message = MIMEMultipart()
     message["From"] = sender_address
@@ -61,61 +61,9 @@ def send_gmail(sender_address, receiver_address, mail_subject, mail_content):
     session.quit()
     print("Sent email to " + receiver_address + " with subject: " + mail_subject)
 
-def process_email(email_data):
-    message = email.message_from_bytes(email_data[0][1])
-    sender = message["From"]
-    subject = message["Subject"]
-    body = ""
-
-    if message.is_multipart():
-        for part in message.get_payload():
-            if part.get_content_type() == "text/plain":
-                body = part.get_payload(decode=True).decode("utf-8")
-                break
-    else:
-        body = message.get_payload(decode=True).decode("utf-8")
-
-    return sender, subject, body
-
-def run_program():
-    process_emails()
-
-# Planifier l'exécution toutes les 5 minutes
-schedule.every(5).minutes.do(run_program)
-
-# Boucle d'exécution principale
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-# Fonction pour extraire les informations personnelles du corps de l'e-mail
-# (Nom, Prénom, Job Title, Nom de l'entreprise, Site internet, Profil LinkedIn, etc.)
-    # Extraction du nom et du prénom
-
-def extract_personal_info(body):
-    nlp = spacy.load("fr_core_news_sm")
-
-    personal_info = {"Last Name": "", "First Name": ""}
-
-    # Extraction du nom et du prénom
-    for entity in doc.ents:
-        if entity.label_ == "PER":
-            name_parts = entity.text.split()
-            if len(name_parts) >= 2:
-                if "Nom" not in personal_info:
-                    personal_info["Nom"] = name_parts[-1]  # Dernier élément du nom
-                elif "Prénom" not in personal_info:
-                    personal_info["Prénom"] = " ".join(name_parts[:-1])  # Tous les éléments précédents
-                else:
-                    break
-
-    return personal_info
-
-
-    
-    # Fonction pour se connecter à la boîte de réception
+# Fonction pour se connecter à la boîte de réception
 def connect_to_mailbox():
-    mailbox = imaplib.IMAP4_SSL("imap.gmail.com")
+    mailbox = imaplib.IMAP4_SSL(IMAP_SERVER)
     mailbox.login(EMAIL_ADDRESS, PASSWORD)
     mailbox.select("INBOX")
     return mailbox
@@ -132,92 +80,35 @@ def fetch_unread_emails(mailbox):
         emails.append(email_message)
     return emails
 
+# Fonction pour extraire les informations personnelles du corps de l'e-mail
+def extract_personal_info(body):
+    nlp = spacy.load("fr_core_news_sm")
+    doc = nlp(body)
+
+    personal_info = {"Nom": "", "Prénom": ""}
 
     for entity in doc.ents:
         if entity.label_ == "PER":
             name_parts = entity.text.split()
             if len(name_parts) >= 2:
-                personal_info["Nom"] = name_parts[-1]  # Dernier élément du nom
-                personal_info["Prénom"] = " ".join(name_parts[:-1])  # Tous les éléments précédents
-
-
-# Se connecter à la boîte de réception
-mailbox = connect_to_mailbox()
-
-# Liste pour stocker les identifiants des e-mails déjà traités
-emails_traites = []
-nouveaux_emails = []
-
-def process_emails():
-    # Votre code pour la récupération des nouveaux e-mails
-    
-    for email in nouveaux_emails:
-        # Récupérer l'identifiant de l'e-mail
-        email_id = get_email_id(email)
-
-        # Vérifier si l'e-mail a déjà été traité
-        if email_id in emails_traites:
-            continue  # Ignorer l'e-mail car il a déjà été traité
-
-        # Vérifier si l'e-mail est indésirable
-        if is_email_indesirable(email):
-            continue  # Ignorer l'e-mail indésirable
-
-        # Traitement de l'e-mail
-        traiter_email(email)
-
-        # Ajouter l'identifiant de l'e-mail à la liste des e-mails traités
-        emails_traites.append(email_id)
-
-        # Autres actions à effectuer après le traitement de l'e-mail
-
-# Fonction pour récupérer l'identifiant de l'e-mail
-def get_email_id(email):
-        sender = get_sender(email)
-        body = get_body(email)
-    # Logique pour extraire l'identifiant de l'e-mail
-    # Retournez l'identifiant de l'e-mail
-        pass
-
-# Fonction pour vérifier si l'e-mail est indésirable
-def is_email_indesirable(email):
-    
-    if "noreply" in email:
-        return True
-    else:
-        return False
-
-    # Logique pour vérifier si l'e-mail est indésirable
-    # Retournez True si l'e-mail est indésirable, False sinon
-
-
-# Parcourir les e-mails et extraire les informations personnelles
-
-def process_emails():
-    for email in emails:
-        email_subject = email["Subject"]
-        email_body = ""
-
-        if email.is_multipart():
-            for part in email.get_payload():
-                if part.get_content_type() == "text/plain":
-                    email_body = part.get_payload(decode=True).decode("utf-8")
+                if "Nom" not in personal_info:
+                    personal_info["Nom"] = name_parts[-1]
+                elif "Prénom" not in personal_info:
+                    personal_info["Prénom"] = " ".join(name_parts[:-1])
+                else:
                     break
-        else:
-            email_body = email.get_payload(decode=True).decode("utf-8")
 
-        personal_info = extract_personal_info(email_body)
+    return personal_info
 
-        print("Sujet:", email_subject)
-        print("Informations personnelles:", personal_info)
-        print("---")
+# Fonction pour extraire le lien LinkedIn de la signature
+def extract_linkedin_url_from_signature(signature):
+    pattern = r"(?i)\b(https?://(?:www\.)?linkedin\.com/\S+)\b"
+    match = re.search(pattern, signature)
+    if match:
+        return match.group(1)
+    return None
 
-        return personal_info
-    
-    # Fonction pour extraire les informations du profil LinkedIn
-    # Envoyer une requête GET pour récupérer le contenu de la page du profil LinkedIn
-    # Analyser le contenu de la page avec BeautifulSoup
-    # Extraire les informations du profil
+# Fonction pour extraire les informations du profil LinkedIn
 def extract_linkedin_profile_info(linkedin_url):
     response = requests.get(linkedin_url)
     if response.status_code == 200:
@@ -225,15 +116,10 @@ def extract_linkedin_profile_info(linkedin_url):
         name = soup.find("li", class_="inline t-24 t-black t-normal break-words").text.strip()
         job_title = soup.find("h2", class_="mt1 t-18 t-black t-normal break-words").text.strip()
         company = soup.find("h3", class_="t-16 t-black t-normal break-words").text.strip()
-
         return name, job_title, company
-
     return None, None, None
 
 # Fonction pour extraire les informations de la signature et du profil LinkedIn
-# Analyser le contenu de l'e-mail et extraire la signature
-# Rechercher un lien LinkedIn dans la signature
-# Extraire les informations du profil LinkedIn
 def extract_info_from_email(email):
     signature = extract_signature_from_email(email)
     linkedin_url = extract_linkedin_url_from_signature(signature)
@@ -245,7 +131,6 @@ def extract_info_from_email(email):
             personal_info["Prénom"] = get_firstname(name)
             personal_info["Titre"] = job_title
             personal_info["Entreprise"] = company
-
     # Autres étapes d'extraction des informations de l'e-mail
     # ...
 
@@ -254,102 +139,80 @@ def extract_signature_from_email(email):
     # Logique d'extraction de la signature de l'e-mail
     # ...
 
-# Fonction pour extraire le lien LinkedIn de la signature
-# Recherche d'un lien LinkedIn dans la signature en utilisant des expressions régulières
-def extract_linkedin_url_from_signature(signature):
-    pattern = r"(?i)\b(https?://(?:www\.)?linkedin\.com/\S+)\b"
-    match = re.search(pattern, signature)
-    if match:
-        return match.group(1)
-    
-    return None
-
 # Fonction pour obtenir le prénom à partir du nom complet
 def get_firstname(fullname):
     parts = fullname.split()
     if len(parts) > 0:
         return parts[0]
-
     return ""
 
 # Générer la réponse en utilisant OpenAI en fonction du prompt et des informations personnelles
 def generate_response(prompt, personal_info):
-    openai.api_key = os.environ["OPENAI_API_KEY"]
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=PROMPT_TO_FOLLOW,
-        temperature=0.9,
-        max_tokens=1024,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        n=1,
+        prompt=prompt,
+        temperature=OPENAI_TEMPERATURE,
+        max_tokens=OPENAI_MAX_TOKENS,
+        top_p=OPENAI_TOP_P,
+        frequency_penalty=OPENAI_FREQUENCY_PENALTY,
+        presence_penalty=OPENAI_PRESENCE_PENALTY,
     )
-    
     generated_text = response.choices[0].text.strip()
-    
     # Ajouter un retour à la ligne après chaque point
     generated_text = ".\n".join(generated_text.split("."))
-    
     return generated_text
 
 # Générer le prompt pour OpenAI en fonction du nom de l'expéditeur et de la question
+
 def generate_prompt(sender_name, question):
-    prompt = "Bonjour"
+    prompt = ""
 
-    sender_firstname = ""  # Valeur par défaut
-
-    # Vérifier si l'information a été extraite de l'e-mail
-    if "sender_firstname" in personal_info:
-        sender_firstname = personal_info["sender_firstname"]
-
-    # Utiliser la variable sender_firstname dans la génération de la réponse
-    prompt = "Bonjour {},\n\n{}\n\n".format(sender_firstname, INTRODUCTION_SENTENCE)
-
-    prompt += "{}\n\n".format(INTRODUCTION_SENTENCE)
-    prompt += "J'ai bien reçu votre question : {}\n\n".format(question)
-    prompt += "Je vais vous fournir une réponse dans les plus brefs délais.\n\n"
-    prompt += "Au plaisir de collaborer avec vous,\n"
-    prompt += "{}".format(BOT_NAME)
+    # Vérifier si le destinataire a déjà reçu un premier email de l'adresse d'expédition
+    if has_received_first_email(sender_name):
+        prompt += "{}\n\n".format(GREETING_SENTENCE)
+        prompt += "J'ai bien reçu votre question : {}\n\n".format(question)
+        prompt += "Je vais vous fournir une réponse dans les plus brefs délais.\n\n"
+        prompt += "Au plaisir de collaborer avec vous,\n"
+        prompt += "{}".format(BOT_NAME)
+    else:
+        prompt += "Bonjour {},\n\n{}\n\n".format(sender_name, INTRODUCTION_SENTENCE)
+        prompt += "{}\n\n".format(INTRODUCTION_SENTENCE)
+        prompt += "J'ai bien reçu votre question : {}\n\n".format(question)
+        prompt += "Je vais vous fournir une réponse dans les plus brefs délais.\n\n"
+        prompt += "Au plaisir de collaborer avec vous,\n"
+        prompt += "{}".format(BOT_NAME)
 
     return prompt
 
 
+# Envoyer une réponse automatique à l'expéditeur
 def send_auto_reply(sender, subject, question, response):
     receiver = sender
     reply_subject = f"Re: {subject}"
-    reply_message = f"Bonjour {sender_firstname},\n\n{response}\n\n{POLITE_CLOSING}\n\nAu plaisir de collaborer avec vous,\n{BOT_NAME}n\n{POST_SCRIPTUM}"
-
+    reply_message = f"Bonjour {personal_info['Prénom']},\n\n{response}\n\n{POLITE_CLOSING}\n\nAu plaisir de collaborer avec vous,\n{BOT_NAME}\n\n{POST_SCRIPTUM}"
     send_gmail(GMAIL_ADDRESS, receiver, reply_subject, reply_message)
 
+# Fonction principale pour traiter les e-mails
 def process_emails():
-    try:
-        mail = imaplib.IMAP4_SSL("imap.gmail.com")
-        mail.login(GMAIL_ADDRESS, GMAIL_APP_PASSWORD)
-        mail.select("inbox")
+    mailbox = connect_to_mailbox()
+    unread_emails = fetch_unread_emails(mailbox)
 
-        while True:
-            typ, email_data = mail.search(None, "UNSEEN")
-            if typ != "OK":
-                continue
+    for email in unread_emails:
+        sender, subject, body = process_email(email)
+        personal_info = extract_personal_info(body)
+        prompt = generate_prompt(personal_info["Prénom"], body)
+        response = generate_response(prompt, personal_info)
+        send_auto_reply(sender, subject, body, response)
 
-            for num in email_data[0].split():
-                typ, email_data = mail.fetch(num, "(RFC822)")
-                if typ != "OK":
-                    continue
+        # Marquer l'e-mail comme lu
+        mailbox.store(email.get("ID"), "+FLAGS", "\\Seen")
 
-                sender, subject, body = process_email(email_data)
-                personal_info = extract_personal_info(body)
-                prompt = generate_prompt(personal_info["Nom"], body)
-                response = generate_response(prompt, personal_info)
-                send_auto_reply(sender, subject, body, response)
+    mailbox.logout()
 
-                # Marquer l'email comme lu
-                mail.store(num, "+FLAGS", "\\Seen")
+# Planifier l'exécution du traitement des e-mails toutes les CHECK_INTERVAL secondes
+schedule.every(CHECK_INTERVAL).seconds.do(process_emails)
 
-            time.sleep(CHECK_INTERVAL)
-
-    finally:
-        mail.logout()
-
-process_emails()
+# Boucle d'exécution principale
+while True:
+    schedule.run_pending()
+    time.sleep(1)
